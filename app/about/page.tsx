56ce711f83550063c068ccff3be0098f8660313e
Abdullah02020 <eng.abdullah.1346@gmail.com>
2025-01-19 21:37:55 +0300

    jidd

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,8 @@
-"use client";
 import AboutHero from "@/components/AboutHero";
 import AboutSection from "@/components/AboutSection";
 import MoreAboutUs from "@/components/MoreAboutUs";
-import Aos from "aos";
-import React, { useEffect } from "react";
 
 const page = () => {
-  // Initialize AOS
-  useEffect(() => {
-    Aos.init({
-      duration: 1200, // Animation duration
-    });
-  }, []);
   return (
     <div>
       <AboutHero />
--- app/zaad-petrol/page.tsx
+++ app/zaad-petrol/page.tsx
@@ -1,17 +1,8 @@
-"use client";
 import PetrolHero from "@/components/PetrolHero";
 import ZaadPetroBenefits from "@/components/ZaadPetroBenefits";
 import ZaadPetroIntro from "@/components/ZaadPetroIntro";
-import Aos from "aos";
-import React, { useEffect } from "react";
 
 const page = () => {
-  // Initialize AOS
-  useEffect(() => {
-    Aos.init({
-      duration: 1200, // Animation duration
-    });
-  }, []);
   return (
     <div>
       <PetrolHero />
